27ffbcafb0e3de30cdecbfd7964fd36949b6cee2
Hans O. Otaño Cuesta <h.otano@concentra.com.do>
2021-05-10 19:47:26 -0400

    Add project files.

---
+++ CleanArchitecture.Domain/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string ISBN { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
